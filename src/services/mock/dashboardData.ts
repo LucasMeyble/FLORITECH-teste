15d51f50fc467b49e8932bf90ff8a3a41493f7d8
LucasMeyble <lucasmeyble123@gmail.com>
2025-05-29 09:41:35 -0300

    chore: add mock data for pie, bar, and line charts

---
+++ src/services/mock/dashboardData.ts
@@ -0,0 +1,5 @@
+export const pieData = [20, 35, 25, 15, 5];
+
+export const barData = [1, 14.9, 17.5, 18, 21, 22, 18, 22, 24, 27];
+
+export const lineData = [1000, 2000, 4000, 3700, 5800, 3950, 2200, 6475, 5925, 7999] 
